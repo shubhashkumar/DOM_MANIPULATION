fe4ddbc7657a1352d69e9d79c88b8a8ccb8c72bc
Shubhash Kumar <subhashsharmandhruv@gmail.com>
2023-12-31 14:31:42 +0530

    get element by id

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("main-heading").innerText="Fruit World";
+document.getElementById("main-heading").style.color="orange";
+document.getElementById("header").style.background="green";
+//document.getElementById("main-heading").style.borderBottomWidth="10px";
+document.getElementById("header").style.borderBottomColor="red";
+document.getElementById("basket-heading").style.color="green";
+document.getElementById("thanks").innerText="Please visit us again";
